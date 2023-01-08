75f64cd93d57a654e277907264370f8b65fe0a03
alibaysarov <alibajsarov353@gmail.com>
2023-01-08 17:37:58 +0300

    Добавил слайдер

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,39 @@
 import * as mainFuncs from "./modules/functions.js";
+
 mainFuncs.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
-const swiper=new Swiper();
+
+const brandSlider=new Swiper('.swiper',{
+  direction: 'horizontal',
+  slidesPerView:4,
+  centeredSlides:true,
+  spaceBetween: 30,
+  // loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay:true,
+  mousewheel: {
+    sensivity:true
+  },
+  speed:500,
+  breakpoints:{
+    992:{
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768:{
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    576:{
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    100:{
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+  }
+})
